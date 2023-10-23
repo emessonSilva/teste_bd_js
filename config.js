24261c5851eb12e1754f0534140e9ab3b903e356
Emesson Silva <140443316+emessonSilva@users.noreply.github.com>
2023-10-23 16:31:32 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+// Exportar as credenciais do banco de dados
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "",
+    "database": "celke",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "",
+    "database": "celke",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "",
+    "database": "celke",
+    "host": "localhost",
+    "dialect": "mysql"
+  }
+}
